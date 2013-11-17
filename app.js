5ca31264562e30e0c8bcd21d6b5c7c93d8130bb1
Patrick Violette <violette.patrick718@gmail.com>
2013-11-17 15:44:14 -0500

    Added stub routes

--- app.js
+++ app.js
@@ -41,7 +41,79 @@ app.get('/users', function(req, res) {
     con.end();
 });
 
+app.get('/login', function(req, res) {
+    res.send("Login lolol");
+})
 
+app.get('/home', function(req, res) {
+    res.send('home');
+});
+
+app.get('/pickups', function(req, res) {
+    res.send('all the pickups');
+});
+
+app.get('/pickup/new', function(req, res) {
+    res.send('the form to pickup a bag');
+});
+
+app.post('/pickups', function(req, res) {
+    // update db with bag pickup
+    res.send('the bag pickup was recorded');
+});
+
+app.post('/dropoffs', function(req, res) {
+    // save new dropoff
+    res.send('created new dropoff');
+});
+
+app.get('/clients', function(req, res) {
+    // list all clients
+    res.send('all the clients');
+});
+
+app.get('/clients/search', function(req, res) {
+    // do le search
+    res.send("this is the client you're looking for.")
+});
+
+app.get('/clients/new', function(req, res) {
+    res.send('form for making new client');
+});
+
+app.post('/clients', function(req, res) {
+    // save the new client from post params
+    res.send('new client added');
+});
+
+app.get('/clients/:clientid/fam/new', function(req, res) {
+    req.params.clientid;
+    res.send('form for addding a new family member');
+});
+
+app.post('/clients/:clientid/fam', function(req, res) {
+    // save the family members
+    res.send('updated the client with the family members');
+});
+
+app.get('/reports/hunger-relief', function(req, res) {
+    res.send('hunger relief report');
+});
+
+app.get('/bags', function(req, res) {
+
+});
+
+
+app.get('/bag/edit', function(req, res) {
+    res.send('the form for editing a bag');
+});
+
+app.put('/bags/:bagname', function(req, res) {
+    // update the bag with new values
+
+    res.send('your bag was updated');
+});
 
 //NEED:
 //Menu, Login, Home, Pickups, Pickup Confirmation, 
