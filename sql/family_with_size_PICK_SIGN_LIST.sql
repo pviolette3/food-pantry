91a9661a8a77a0ce513ef81a9f9e745c2eba1e48
Patrick Violette <violette.patrick718@gmail.com>
2013-12-05 05:11:21 -0500

    removed extra sql files

--- sql/family_with_size_PICK_SIGN_LIST.sql
+++
@@ -1,6 +0,0 @@
-SELECT c.CID, 
-    CASE WHEN fam.family_size IS NOT NULL 
-        THEN fam.family_size + 1 
-        ELSE 1
-    END AS Size, c.* FROM (SELECT c1.CID AS CID, COUNT(c1.CID) AS family_size FROM (SELECT c.CID AS CID FROM Client c, FamilyMember f where c.CID = f.ClientID) AS c1
-GROUP BY CID) as fam RIGHT OUTER JOIN Client c ON fam.CID = c.CID;
